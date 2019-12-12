5786e5fad8eff8d047b3432251371b0c9ca61f86
Yash Kalebere <ysk25@drexel.edu>
2019-12-12 15:32:30 -0500

    Extend FT for input to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 @pytest.fixture
 def new_browser():
@@ -15,14 +17,24 @@ def test_can_start_a_list_and_retrieve_it_later(new_browser):
 
     # She notices the page title and header mention to-do lists
     assert 'To-Do' in browser.title
+    header_text = browser.find_element_by_tag_name('h1').text
+    assert 'To-Do' in header_text
 
     # She is invited to enter a to-do item straight away
+    input_box = browser.find_element_by_id('id_new_item')
+    assert input_box.get_attribute('placeholder') == 'Enter a to-do item'
 
     # She types "Buy peacock feathers" into a text box (Edith's hobby
     # is tying fly-fishing lures)
+    input_box.send_keys('Buy peacock feathers')
 
     # When she hits enter, the page updates, and now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list
+    input_box.send_keys(Keys.ENTER)
+
+    table = browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    assert any(row.text == '1: Buy peacock feathers' for row in rows)
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
