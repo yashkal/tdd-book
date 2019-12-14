9c305752ff38aedb3f58a501898db3cf9101b15f
Yash Kalebere <ysk25@drexel.edu>
2019-12-14 01:57:02 -0500

    Simplify home page template test

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,5 @@
 from lists.views import home_page
 
-def test_home_page_returns_correct_html(client):
+def test_uses_home_template(client):
     response = client.get('/')
-
-    html = response.content.decode('utf8')
-    assert '<html>' in html
-    assert '<title>To-Do lists</title>' in html
-    assert html.strip().endswith('</html>')
-
     assert 'home.html' in (t.name for t in response.templates)
