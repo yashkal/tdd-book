29c5d27c49b738ee479dd5458f20687504b4fb7b
Yash Kalebere <ysk25@drexel.edu>
2019-12-14 23:46:00 -0500

    Extend FT to test multiple lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,6 @@
 import time
 import pytest
+import re
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -29,7 +30,7 @@ def test_can_start_a_list_and_retrieve_it_later(new_browser, live_server):
     # Edith has heard about a cool new online to-do app. She goes
     # to check out its homepage
     browser = new_browser
-    browser.get(live_server.url + '/')
+    browser.get(live_server.url)
 
     # She notices the page title and header mention to-do lists
     assert 'To-Do' in browser.title
@@ -47,8 +48,6 @@ def test_can_start_a_list_and_retrieve_it_later(new_browser, live_server):
     # When she hits enter, the page updates, and now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list
     input_box.send_keys(Keys.ENTER)
-    time.sleep(2)
-
     wait_for_row_in_list_table(browser, '1: Buy peacock feathers')
 
     # There is still a text box inviting her to add another item. She
@@ -56,18 +55,50 @@ def test_can_start_a_list_and_retrieve_it_later(new_browser, live_server):
     input_box = browser.find_element_by_id('id_new_item')
     input_box.send_keys('Use peacock feathers to make a fly')
     input_box.send_keys(Keys.ENTER)
-    time.sleep(2)
 
+    # The page updates again, and now shows both items on her list
     wait_for_row_in_list_table(browser, '1: Buy peacock feathers')
     wait_for_row_in_list_table(browser, '2: Use peacock feathers to make a fly')
 
-    # The page updates again, and now shows both items on her list
-
-    # Edith wonders whether the site will remember her list. Then she sees
-    # that the site has generated a unique URL for her -- there is some
-    # explanatory text to that effect.
-
-    # She visits that URL - her to-do list is still there.
-
     # Satisfied, she goes back to sleep
-    pytest.fail('Finish this test')
+
+def test_multiple_users_can_start_lists_at_different_urls(new_browser, live_server):
+    # Edith starts a new to-do list
+    browser = new_browser
+    browser.get(live_server.url)
+    inputbox = browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Buy peacock feathers')
+    inputbox.send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(browser, '1: Buy peacock feathers')
+
+    # She notices that her list has a unique URL
+    edith_list_url = browser.current_url
+    assert re.match('/lists/.+', edith_list_url)
+
+    # Now a new user, Francis, comes along to the site
+    browser.quit()
+    browser = new_browser
+
+    # There is no sign of Edith's list
+    browser.get(live_server.url)
+    page_text = browser.find_element_by_tag_name('body').text
+    assert 'Buy peacock feathers' not in page_text
+    assert 'make a fly' not in page_text
+
+    # Francis starts a new list
+    inputbox = browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Buy milk')
+    inputbox.send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(browser, '1: Buy milk')
+
+    # Francis gets his own url
+    francis_list_url = browser.current_url
+    assert re.match('/lists/.+', edith_list_url)
+    assert edith_list_url != francis_list_url
+
+    # There is still no sign of Edith's list
+    page_text = browser.find_element_by_tag_name('body').text
+    assert 'Buy peacock feathers' not in page_text
+    assert 'make a fly' not in page_text
+
+    # Satisfied, they both go to sleep
