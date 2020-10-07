95eef516236d6b1b4acb492ca86ee69096c351e6
Yash Kalebere <ysk25@drexel.edu>
2020-10-06 23:21:09 -0400

    Move and split lists app unit tests into folder

--- app/lists/tests/test_views.py
+++ app/lists/tests/test_views.py
@@ -10,36 +10,6 @@ class TestHomePage:
 
 
 @pytest.mark.django_db
-class TestListAndItemModel:
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        assert saved_list == list_
-
-        saved_items = Item.objects.all()
-        assert saved_items.count() == 2
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        assert first_saved_item.text == "The first (ever) list item"
-        assert first_saved_item.list == list_
-        assert second_saved_item.text == "Item the second"
-        assert second_saved_item.list == list_
-
-
-@pytest.mark.django_db
 class TestListView:
     def test_uses_list_template(self, client):
         list_ = List.objects.create()
