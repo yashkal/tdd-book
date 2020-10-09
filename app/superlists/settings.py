c90984f005c7fb12320ae9b9ef16bf050bc50d74
Yash Kalebere <ysk25@drexel.edu>
2020-10-09 03:14:11 -0400

    Refactor docker-compose and .env files
    - Remove production env_file from git
    - Allow `ALLOWED_HOSTS` environment variable to be empty for development
    - Combine docker-compose.yml and docker-compose.override.yml into one

--- app/superlists/settings.py
+++ app/superlists/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+try:
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+except AttributeError:
+    pass
 
 # Application definition
 
