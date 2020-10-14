02b0d5635ea76f4cb03d9d6fcc6debde66a0c145
Yash Kalebere <ysk25@drexel.edu>
2020-10-14 15:48:13 -0400

    Remove class boilerplate

--- app/lists/tests/test_models.py
+++ app/lists/tests/test_models.py
@@ -2,43 +2,45 @@ import pytest
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
+pytestmark = pytest.mark.django_db
 
-@pytest.mark.django_db
-class ListAndItemModelTest:
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
 
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = list_
-        first_item.save()
+def test_saving_and_retrieving_items():
+    list_ = List()
+    list_.save()
 
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = list_
-        second_item.save()
+    first_item = Item()
+    first_item.text = "The first (ever) list item"
+    first_item.list = list_
+    first_item.save()
 
-        saved_list = List.objects.first()
-        assert saved_list == list_
+    second_item = Item()
+    second_item.text = "Item the second"
+    second_item.list = list_
+    second_item.save()
 
-        saved_items = Item.objects.all()
-        assert saved_items.count() == 2
+    saved_list = List.objects.first()
+    assert saved_list == list_
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        assert first_saved_item.text == "The first (ever) list item"
-        assert first_saved_item.list == list_
-        assert second_saved_item.text == "Item the second"
-        assert second_saved_item.list == list_
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
 
-    def test_cannot_save_empty_list_items(self):
-        list_ = List.objects.create()
-        item = Item(list=list_, text="")
-        with pytest.raises(ValidationError):
-            item.save()
-            item.full_clean()
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+    assert first_saved_item.text == "The first (ever) list item"
+    assert first_saved_item.list == list_
+    assert second_saved_item.text == "Item the second"
+    assert second_saved_item.list == list_
 
-    def test_get_absolute_url(self):
-        list_ = List.objects.create()
-        assert list_.get_absolute_url(), f"/lists/{list_.id}/"
+
+def test_cannot_save_empty_list_items():
+    list_ = List.objects.create()
+    item = Item(list=list_, text="")
+    with pytest.raises(ValidationError):
+        item.save()
+        item.full_clean()
+
+
+def test_get_absolute_url():
+    list_ = List.objects.create()
+    assert list_.get_absolute_url(), f"/lists/{list_.id}/"
