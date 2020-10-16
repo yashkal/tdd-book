9008441b5b3490f6c8c101887470cca884b8c541
Yash Kalebere <ysk25@drexel.edu>
2020-10-16 00:32:32 -0400

    Add FT for no duplicate items

--- app/functional_tests/test_list_item_and_validation.py
+++ app/functional_tests/test_list_item_and_validation.py
@@ -33,3 +33,21 @@ def test_cannot_add_empty_list_items(new_browser):
     get_item_input_box(browser).send_keys(Keys.ENTER)
     wait_for_row_in_list_table(browser, "1: Buy milk")
     wait_for_row_in_list_table(browser, "2: Make tea")
+
+
+def test_cannot_add_duplicate_items(new_browser):
+    # Edith goes to the homepage and starts a new list
+    browser = new_browser()
+    get_item_input_box(browser).send_keys("Buy wellies")
+    get_item_input_box(browser).send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(browser, "1: Buy wellies")
+
+    # She accidently tries to add a duplicate item
+    get_item_input_box(browser).send_keys("Buy wellies")
+    get_item_input_box(browser).send_keys(Keys.ENTER)
+
+    # She sees a helpful error message
+    assert (
+        wait_for(lambda: browser.find_element_by_css_selector(".has-error").text)
+        == "You've already got this in your list"
+    )
