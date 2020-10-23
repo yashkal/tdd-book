9c8bc4fe7f12d9f7de2c5fa642ad56a657b9e4af
Yash Kalebere <ysk25@drexel.edu>
2020-10-23 13:41:29 -0400

    Refactor and add FT for clearing error element
    - Test for error element cleared on text input
    - Define helper function to find error element

--- app/functional_tests/test_list_item_and_validation.py
+++ app/functional_tests/test_list_item_and_validation.py
@@ -3,6 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from helpers import get_item_input_box, wait_for, wait_for_row_in_list_table
 
 
+def get_error_element(browser):
+    return browser.find_element_by_css_selector(".has-error")
+
+
 def test_cannot_add_empty_list_items(new_browser):
     # Edith goes to the home page and accidently tries to submit and empty list
     # item. She hits enter on the empty input box
@@ -48,6 +52,25 @@ def test_cannot_add_duplicate_items(new_browser):
 
     # She sees a helpful error message
     assert (
-        wait_for(lambda: browser.find_element_by_css_selector(".has-error").text)
+        wait_for(lambda: get_error_element(browser).text)
         == "You've already got this in your list"
     )
+
+
+def test_error_messages_are_cleared_on_input(new_browser):
+    browser = new_browser()
+
+    # Edith starts a new list and causes a validation error
+    get_item_input_box(browser).send_keys("Banter too thick")
+    get_item_input_box(browser).send_keys(Keys.ENTER)
+    wait_for_row_in_list_table(browser, "1: Banter too thick")
+    get_item_input_box(browser).send_keys("Banter too thick")
+    get_item_input_box(browser).send_keys(Keys.ENTER)
+
+    assert wait_for(lambda: get_error_element(browser).is_displayed())
+
+    # She starts typing in the input box to clear the error
+    get_item_input_box(browser).send_keys("a")
+
+    # She is pleased to see the error message disappear
+    assert wait_for(lambda: get_error_element(browser).is_displayed()) is False
